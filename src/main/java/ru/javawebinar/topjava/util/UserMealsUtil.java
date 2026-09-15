15581e6b54c4d12e0c267d944f9f80f635f41366
MAGNAT\sergey_ra <rakcheev.sg@magnat.ru>
2026-09-15 21:34:00 +0300

    codestyle: Update method getUserMealWithExcess

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -71,10 +71,10 @@ public class UserMealsUtil {
     }
 
     private static UserMealWithExcess getUserMealWithExcess(UserMeal meal, Map<LocalDate, Integer> totalCaloriesPerDay, int caloriesPerDay) {
-        LocalDate localDate = meal.getDate();
-        boolean isExxess = totalCaloriesPerDay.getOrDefault(localDate, 0) > caloriesPerDay;
+        LocalDate mealLocalDate = meal.getDate();
+        boolean isExcess = totalCaloriesPerDay.getOrDefault(mealLocalDate, 0) > caloriesPerDay;
         return new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(),
-                isExxess);
+                isExcess);
     }
 
 }
